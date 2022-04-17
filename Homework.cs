9c383fc214d77ea37658820c5caa2c6c362ff46e
Natalia <iamnataliyaa@gmail.com>
2022-04-17 17:48:43 +0300

    First Home work c#

--- Homework.cs
+++ Homework.cs
@@ -3,15 +3,55 @@
 a = 5; b = 7 -> max = 7
 a = 2 b = 10 -> max = 10
 a = -9 b = -3 -> max = -3
-Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//Решение
+Console.WriteLine("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+    Console.WriteLine("First number is biger then second number");
+else
+    Console.WriteLine("First number is less than second number");
+
+//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22
+//Решение
+Console.WriteLine("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num1 > num2 && num1 > num3) max = num1;
+if (num2 > num3 && num2 > num1) max = num2;
+if (num3 > num1 && num3 > num2) max = num3;
+Console.Write("Max = ");
+Console.WriteLine(max);
+/*
 Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 4 -> да
 -3 -> нет
 7 -> нет
-Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-5 -> 2, 4
-8 -> 2, 4, 6, 8
+//Решение
+Console.WriteLine("Input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 % 2 == 0)
+Console.WriteLine("Number is even");
+else
+Console.WriteLine("Number is NOT even");
 */
+//Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+//Решение
+Console.Write("Input integer number: ");4
+int num = Convert.ToInt32(Console.ReadLine());
+int current = 1;
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
