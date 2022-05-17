57cff600d44d05abb37263e1b94d89b6e831f4fa
Natalia <iamnataliyaa@gmail.com>
2022-05-17 21:22:25 +0300

    Home work 7

--- Homework7/Homework7.cs
+++ Homework7/Homework7.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+double[,]CreateRandomArray(
+    int m,
+    int n)
+{
+    double[,] array = new double [m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            array[i,j] = new Random().NextDouble()*100 - 10;
+            Console.Write("{0,6:F2}", array[i,j]);
+        }
+        Console.WriteLine();
+    }       
+    return array;
+}
+Console.Write("Input m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+CreateRandomArray(m,n);
+
+//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
