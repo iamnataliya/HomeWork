db113b3664fd3416de3100ad4a978e9352ff8e9a
Natalia <iamnataliyaa@gmail.com>
2022-05-20 22:07:13 +0300

    Home work8

--- Homework8/Homework8.cs
+++ Homework8/Homework8.cs
@@ -1,5 +1,59 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
 
+Console.Clear();
+int[,] GetMatrix(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+    return matrix;
+}
+int[,] SortRowAscending(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            int minPosition = j;                                            
+            for (int k = j + 1; k < array.GetLength(1); k++)                
+            {
+                if (array[i, k] > array[i, minPosition]) minPosition = k;    
+            }
+            int tmp = array[i, j];                                          
+            array[i, j] = array[i, minPosition];                            
+            array[i, minPosition] = tmp;                                    
+        }
+
+    }
+    return array;
+}
+void PrintTable(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "  ");
+        }
+        System.Console.WriteLine();
+    }
+}
+int row = 4;
+int col = 7;
+int min = 10;
+int max = 100;
+
+int[,] table = GetMatrix(row, col, min, max);
+PrintTable(table);
+
+System.Console.WriteLine();
+int[,] rowSort = SortRowAscending(table);
+PrintTable(rowSort);
 
 //Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 //Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
