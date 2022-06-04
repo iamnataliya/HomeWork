d91eb468c821c37b560ae3f9563ea869ab5ec002
Natalia <iamnataliyaa@gmail.com>
2022-06-04 12:30:21 +0300

    Correct

--- Homework10/Homework10.cs
+++ Homework10/Homework10.cs
@@ -1,12 +1,27 @@
 ﻿// Задача 1: Задайте массив строк. Напишите программу, считает кол-во слов в массиве, начинающихся на гласную букву.
 // Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
 
-Console.Write("Write text: ");
-string[] textMass;
-var text = Console.ReadLine();
-textMass = text.Split(' ');
-Console.Write(value: "Number of words in the text: ");
-Console.WriteLine(textMass.Length);
-Console.ReadLine();
+// Console.Write("Write text: ");
+// string[] textMass;
+// var text = Console.ReadLine();
+// textMass = text.Split(' ');
+// Console.Write(value: "Number of words in the text: ");
+// Console.WriteLine(textMass.Length);
+// Console.ReadLine();
 // Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
 // Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
+
+string [] SortStrings(string[] words)
+{
+    for (int i = 0; i < words.Length ; i+=2)
+    {
+        words[i] = words[i] + words[i+1];
+    }
+    return words;
+}
+string[] names = { "qwe", "wer", "ert", "rty", "tyu", "yui"};
+SortStrings(names);
+for (int i = 0; i < names.Length; i+=2)
+{
+    Console.Write(names[i] + " ");
+}
